4e14a2241eec134170790f077d0d4cfbf3ab9f76
Masum <masumfromjessore@gmail.com>
2019-06-02 00:58:19 +0600

    f

---
+++ maslog/src/main/java/com/crezyprogrammer/maslog/MasLog.java
@@ -0,0 +1,12 @@
+package com.crezyprogrammer.maslog;
+
+import android.util.Log;
+
+public class MasLog {
+    public static  void auto(String text){
+        Log.i("123321",text);
+    }
+    public  static  void  menu(String tag,String text){
+        Log.i(tag,text);
+    }
+}
